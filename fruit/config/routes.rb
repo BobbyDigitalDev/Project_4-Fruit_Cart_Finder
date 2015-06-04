f0be51486f8219978d4376d9c947cbd27d1f2592
Bobby Lopez <bobbyl76@gmail.com>
2015-06-04 11:13:11 -0400

    am progress

--- fruit/config/routes.rb
+++ fruit/config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
 
-  root 'sessions#new'
+  root 'welcome#index'
+  # root 'sessions#new'
+  resources :users
 
-  resource :users
+  resources :sessions, only: [:new, :create, :destroy]
 
-  resource :sessions, only: [:new, :create, :destroy]
+  resources :carts
 
-  
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
